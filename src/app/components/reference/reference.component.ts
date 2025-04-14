6947ff43bb50afbcabb0c8c8ef398de3aa409ee6
Martin Sinner <m-sinner@web.de>
2025-04-14 22:09:24 +0200

    added reference section

--- src/app/components/reference/reference.component.ts
+++ src/app/components/reference/reference.component.ts
@@ -1,12 +1,41 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-reference',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './reference.component.html',
   styleUrl: './reference.component.scss'
 })
 export class ReferenceComponent {
+  reference = [
+    {
+      title: 'David Gerlitz',
+      project: 'Join',
+      description: 'Martin is a reliable and supportive team player. He contributes great ideas, works solution-oriented,and ensures a pleasant collaboration.',
+      shape: ['reference-shape.png']
+    },
 
+    {
+      title: 'Patrick Glißmann',
+      project: 'El Pollo Loco',
+      description: 'Working with Martin was consistently positive. He is especially characterized by his reliable way of working and his strong willingness to learn and adopt new concepts.',
+      shape: ['reference-shape.png']
+    },
+
+    {
+      title: 'Rui Pinto',
+      project: 'Pinto Cases',
+      description: 'Martin modernized my website and gave it a completely fresh look. Working with him was straightforward, his ideas for implementation were great –I am really happy with the result.',
+      shape: ['reference-shape.png']
+    }
+  ]
+
+
+  activeIndex = 0;
+
+  setActiveProject(index: number) {
+      this.activeIndex = index;
+  }
 }
